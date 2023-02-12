edca396de2ab64486e800b4636bb3fcf368a8f62
Fabio Teichmann <fabio.teichmann@icloud.com>
2023-02-12 11:33:05 +0200

    added insert() to linked list

--- JavaScript/linked-list/linked-list.js
+++ JavaScript/linked-list/linked-list.js
@@ -8,20 +8,12 @@ class Node {
 class LinkedList {
     constructor(value) {
         this.head = new Node(value);
-        // this.head = {
-        //     value: value,
-        //     next: null,
-        // }
         this.tail = this.head;
         this.length = 1;
     }
 
     append(value) {
         const node = new Node(value);
-        // const node = {
-        //     value: value,
-        //     next: null,
-        // };
         this.tail.next = node;
         this.tail = node;
         this.length++;
@@ -30,23 +22,53 @@ class LinkedList {
 
     prepend(value) {
         const node = new Node(value);
-        // const node = {
-        //     value: value,
-        //     next: null,
-        // };
         node.next = this.head;
         this.head = node;
         this.length++;
         return this;
     }
 
+    insert(index, value) {
+        // inserts value at index, i.e. before the element that was at index before.
+        // Indeces start at 0 (head).
+        // if index >= length of linked list, the element will be appended at the end
+        // of the list.
+        if (index >= this.length - 1) {
+            this.append(value);
+        } else if (index === 0) {
+            this.prepend(value);
+        } else {
+            let temp = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            const node = new Node(value);
+            node.next = temp.next;
+            temp.next = node;
+            this.length++;
+        }
+    }
+
+    printList() {
+        const array = [];
+        let n = this.head;
+        while (n !== null) {
+            array.push(n.value);
+            n = n.next;
+        }
+        return array;
+    }
+
 }
 
 const myLinkedList = new LinkedList(10);
-console.log(myLinkedList)
+// console.log(myLinkedList)
 
 myLinkedList.append(5);
 // myLinkedList.append(7);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+
+myLinkedList.insert(2, 77);
+console.log(myLinkedList.printList());
+// console.log(myLinkedList);
